499efb9a6c2576e26334b45727ef76afc66d29c6
sOkam <7308253+heysokam@users.noreply.github.com>
2023-12-16 09:49:06 -0800

    fix: Union codegen and Preprocessor double-include

--- src/minc/prep.nim
+++ src/minc/prep.nim
@@ -1,6 +1,9 @@
 #:______________________________________________________
 #  ᛟ minc  |  Copyright (C) Ivan Mar (sOkam!)  |  MIT  :
 #:______________________________________________________
+## @fileoverview
+## Defines the minc compiler preprocessor functionality
+#_______________________________________________________|
 # @dependencies std
 import std/strutils
 import std/paths
@@ -10,24 +13,24 @@ import ./logger
 
 
 func isInclude (line :string) :bool=  line.startsWith("include") and '\"' notin line and '<' notin line and '>' notin line
-  ## Checks if the line contains a file include that should be processed
-  ## Lines that match start with `include` and contain a path without any " < > characters
+  ## @description Checks if the line contains a file include that should be processed
+  ## @notes Lines that match start with `include` and contain a path without any " < > characters
 
 var includedFiles :HashSet[string]
-  ## Processed include files state
+  ## @description Processed include files state
 proc getInclude (line :string; root :Path) :tuple[code:string, root:Path]=
-  ## Gets the contents of the file referenced by the include line
-  ## The compiler logger object must be initialized before running this function.
-  let path    = line[8..^1].strip().strip( leading=false, chars={'\n'} ).Path
-  let file    = root/path.addFileExt("cm")
+  ## @description Gets the contents of the file referenced by the include line
+  ## @requires The compiler logger object must be initialized before running this function.
+  let path     :Path= line[8..^1].strip().strip( leading=false, chars={'\n'} ).Path
+  let file     :Path= root/path.addFileExt("cm")
+  let included :bool= includedFiles.containsOrIncl(file.string)
+  if included: dbg "Skipped recursive include for file: ", file.string; return
   result.root = file.splitFile.dir
-  try         : result.code = readFile(file.string)
-  except      : fail "Tried to include a file, but failed reading it.\n  ", file.string
-  finally     :
-    if includedFiles.containsOrIncl(file.string): dbg "Skipped recursive include for file: ", file.string
+  try    : result.code = readFile(file.string)
+  except : fail "Tried to include a file, but failed reading it.\n  ", file.string
 
 proc processIncludes *(code :string; root :Path) :string=
-  ## Recursively add all of the includes that should be preprocessed
+  ## @description Recursively add all of the includes that should be preprocessed
   for line in code.splitLines:
     if line.isInclude:
       var tmp = line.getInclude(root)
