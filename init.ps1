2644d7fddbce4840f2654455a00b33926d265e4c
sOkam <7308253+heysokam@users.noreply.github.com>
2023-12-27 03:27:31 +0100

    bld: Scripts for cloning and simpler initialization

--- init.ps1
+++ init.ps1
@@ -1,22 +1,56 @@
 #!/usr/bin/env sh
-# @file
-# Combined Shell and PowerShell initialization script.
-# It's only goal is to build a local copy of Nim
-# that is then used to run the .nims files that build the MinC compiler.
-Prefix="ᛟ minc:"
-echo --% >/dev/null;: ' | out-null
-<#'
+# @fileoverview
+#  Combined Shell and PowerShell initialization script.
+#  It's only goal is to build a local copy of Nim
+#  that is then used to run the .nims files that build the MinC compiler.
+nimMaj=2          # Major nim version that MinC tracks
+nimMin=0          # Minor nim version that MinC tracks
+Prefix="ᛟ minc:"  # Prefix to append to all console info messages
+
 #_______________________________________
 # Shell Initialization
 #____________________________
 # Don't use  #＞ directly  (e.g.  #"">   or similar).
-# Otherwise the Shell section would be exited and Shell would try to execute Powershell code.
+# Otherwise the Bash section would be exited and Bash would try to execute Powershell code.
+echo --% >/dev/null;: ' | out-null
+<#'
+#____________________________
+# Bash tools
 set -u # error on undefined variables
 set -e # exit on first error
-echo "$Prefix Running Unix Shell init section..."
-thisDir=$(dirname $(realpath $BASH_SOURCE))
-sh $thisDir/src/build/init.sh
-echo "$Prefix Finished Unix Shell init."
+# Project setup
+thisDir=$(dirname "$(realpath "$BASH_SOURCE")")
+rootDir="$thisDir"        # Find the root folder of the repository
+binDir="$rootDir/bin"     # Binaries output folder
+nimDir="$binDir/nim"      # nim submodule folder (absolute)
+nimBranch="version-$nimMaj-$nimMin"  # Name of the Nim branch that MinC tracks
+nim="$nimDir/bin/nim"     # Nim binary result after it has been bootstrapped
+echo "$Prefix Initializing MinC binaries from Bash ..."
+
+#____________________________
+# Clean binDir
+wrn="$Prefix Warning-> This will delete everything at $binDir . Continue?"$'\n'$"  [Recommended (yes)]  (y/n)? "
+read -rep "$wrn" -n 1 answer
+case "$answer" in
+  "y" | "Y" | "" )
+    echo "$Prefix Cleaning all content of $binDir ..."
+    rm -rfv $binDir
+    mkdir -v $binDir
+    echo -e "*\n!.gitignore" > "$binDir"/.gitignore
+    ;;
+  "n" | "N" | * ) echo "$Prefix Continuing without removing $dir" ;;
+esac
+
+#____________________________
+# Init nimDir
+prevDir=$PWD
+git clone --depth=1 -b $nimBranch https://github.com/nim-lang/Nim.git $nimDir
+cd $nimDir
+sh ./build_all.sh
+cd $prevDir
+
+echo "$Prefix Finished initializing MinC binaries."
+#____________________________
 # end bash section
 exit #>
 #____________________________
