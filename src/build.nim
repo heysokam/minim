515caf906e96f8d275be5d5c6f9ca8c12724c1ef
sOkam <7308253+heysokam@users.noreply.github.com>
2023-12-31 18:13:12 +0100

    bld: Better init system without atlas or bash. Using nim instead

--- src/build.nim
+++ src/build.nim
@@ -3,18 +3,22 @@
 #:______________________________________________________
 # std dependencies
 import std/os except `/`
+import std/[ strformat,strutils ]
 # confy dependencies
-when not defined(nimble):
-  import ../../libs/confy/src/confy
-else:
-  import confy
+import confy
 
-cfg.zigSystemBin = off
+let nimDir          = cfg.binDir/".nim"
+let libdir          = cfg.srcDir/"lib"
+let nstdPath        = libDir/"nstd"/"src"
+let slatePath       = libDir/"slate"/"src"
+cfg.zigcc.systemBin = off
+cfg.nim.cc          = string( nimDir/"bin"/"nim" )
 
 var mincGen = Program.new(cfg.srcDir/"minc"/"gen"/"proto.nim", "mincGen")
-os.removeFile( cfg.binDir/mincGen.trg )
+os.removeFile( string cfg.binDir/mincGen.trg )
 mincGen.build( @[mincGen.trg.string], run=true, force=true )
 
-var minc = Program.new(cfg.srcDir/"minc.nim", "minc")
-os.removeFile( cfg.binDir/minc.trg )
+var minc = Program.new(cfg.srcDir/"minc.nim", "minc", args="$1 $2 $3" % [
+  "--noNimblePath", &"--path:{nstdPath}", &"--path:{slatePath}" ])
+os.removeFile( string cfg.binDir/minc.trg )
 minc.build( @["all", minc.trg.string], run=false, force=false )
