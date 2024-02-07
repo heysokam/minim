db4e2f1fb180dbd4569ddbb7120de04fc6d32510
sOkam <7308253+heysokam@users.noreply.github.com>
2024-02-07 05:56:44 +0100

    bld: Explicit disable verbose for building the minc compiler

--- src/runic.nim
+++ src/runic.nim
@@ -5,3 +5,5 @@
 #      : https://github.com/PMunch/futhark/blob/master/src/opir.nim#L425
 # idea : Convert clang.CXCursor to PNodes directly
 #      : clang bindings
+# remember: Alternative to libclang
+# https://github.com/RealNeGate/Cuik/tree/master/libCuik
