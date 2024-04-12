1ded59e9dc85bfa02aa2ba44e295e2d2665da5d0
sOkam <7308253+heysokam@users.noreply.github.com>
2024-04-12 02:49:29 +0200

    bld: Unit Tests: Compilation sketch

--- tests/t001/test.nim
+++ tests/t001/test.nim
@@ -1,8 +1,9 @@
 const Title = "Category"
 #_______________________________________
 # @deps tests
-template tName:string= currentSourcePath.parentDir.lastPathPart()
 include ../base
+const thisDir = currentSourcePath.parentDir()
+template tName:string= thisDir.lastPathPart()
 #_______________________________________
 
 
@@ -10,4 +11,6 @@ include ../base
 # @section Test
 #_____________________________
 test name "correct sum": check 5+5 == 10
-
+test name "dummy check 2":
+  let code = compile thisDir/"entry.cm"
+  check code == readFile(thisDir/"ref.c")
