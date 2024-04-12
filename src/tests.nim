74e532913275583d8dd8e6d2d5b7a47fc4c67786
sOkam <7308253+heysokam@users.noreply.github.com>
2024-04-12 03:20:40 +0200

    bld: Better logger. Build minc everytime the tests are built

--- src/tests.nim
+++ src/tests.nim
@@ -1,14 +1,21 @@
 # @deps ndk
 import confy
+import nstd/logger
+import ./minc/cfg as minc
 
 #_______________________________________
 # Configuration
 cfg.libDir        = cfg.srcDir/"lib"
 cfg.srcDir        = cfg.rootDir/"tests"
 cfg.nim.systemBin = off
+logger.init(minc.Prefix)
 
 #_______________________________________
-# Build
+# Build: MinC
+withDir rootDir: sh "spry build"
+
+#_______________________________________
+# Build: Test Runner
 build Program.new(
   src  = cfg.srcDir/"tMinC.nim",
   deps = Dependencies.new(
