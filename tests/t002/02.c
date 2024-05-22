e786edfd54d70489d4d1ae6b667a0a39c45b18b2
sOkam <7308253+heysokam@users.noreply.github.com>
2024-05-22 02:47:23 +0200

    tst: UnitTests for variables: `let`, `var` and `{.persist.}`

--- tests/t002/02.c
+++ tests/t002/02.c
@@ -1,1 +1,6 @@
 static /*constexpr*/ int const forty2 = 42;
+static /*constexpr*/ int const one    = 1;
+static /*constexpr*/ int const two    = 2;
+static /*constexpr*/ int const three  = 3;
+static /*constexpr*/ int const four   = 4;
+static /*constexpr*/ int const five   = 5;
