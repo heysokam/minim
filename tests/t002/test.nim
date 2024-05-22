beadb733ae8625ef27f718016e43155902939b2e
sOkam <7308253+heysokam@users.noreply.github.com>
2024-05-22 07:24:57 +0200

    rew: Array support for variables. chg: Better variables and errors

--- tests/t002/test.nim
+++ tests/t002/test.nim
@@ -15,4 +15,11 @@ test name "Const: Private definition" : check "02"
 test name "Let: Private definition"   : check "03"
 test name "Var: Private definition"   : check "04"
 test name "Pragma: Persist"           : check "05"
+test name "Arrays: Definition"        : check "06"
+
+#  TODO: Convert  _  to  {0}  and  {}
+#  TODO: Multiline strings
+#  TODO: chars
+#  TODO: Array Value
+#  TODO: Object Value
 
