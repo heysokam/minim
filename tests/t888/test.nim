f1db19e5751bd29618b36c0c32593823301bdd3f
sOkam <7308253+heysokam@users.noreply.github.com>
2024-06-03 16:16:03 +0200

    tst: Folder skeleton for `examples/eNN*`

---
+++ tests/t888/test.nim
@@ -0,0 +1,13 @@
+const Title = "Examples"
+#_______________________________________
+# @deps tests
+include ../base
+const thisDir = currentSourcePath.Path.parentDir()
+template tName:Path= thisDir.lastPathPart()
+#_______________________________________
+
+
+#_______________________________________
+# @section Test
+#_____________________________
+# test name "Basic Code Generation" : check "00"
