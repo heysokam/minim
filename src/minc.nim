71be14d0ae34d61471495444d6072e95953d0977
sOkam <7308253+heysokam@users.noreply.github.com>
2024-06-08 10:03:23 +0200

    chg: Intermediate cache file for the preprocessor step: `.ref.cm`

--- src/minc.nim
+++ src/minc.nim
@@ -20,22 +20,30 @@ when isMainModule:
   let cli = opts.init()
   if cli.cmd in {Compile, Codegen}:
     block SectionBin:
-      let relFile   = cli.output.changeFileExt("c")
+      #_____________________________
+      # File Configuration
+      let relFile   = cli.output.changeFileExt("c").lastPathPart
       let cacheFile = cli.cacheDir/relFile
       let rawFile   = cacheFile.changeFileExt(".raw.c")
+      let refFile   = cacheFile.changeFileExt(".ref.cm")
       let binFile   = cli.binDir/cli.output
+      #_____________________________
       # Preprocess
       let root = cli.input.splitFile.dir
       let code = cli.input.readFile()
         .processIncludes(root, cli.input)  # Preprocess includes
-      # Codegen the C code
-      rawFile.writeFile convert.toMinC_singleFile(code)
+      #_____________________________
+      # Codegen
+      md cli.cacheDir                                   # Create the .cache dir if it doesn't exist
+      refFile.writeFile code                            # Write the preprocessor generated code into a reference file
+      rawFile.writeFile convert.toMinC_singleFile(code) # Codegen the C code
       cacheFile.writeFile format.clang(
         cli  = cli,
         code = rawFile.readFile, # TODO: Support for Multi-file
         ) # << format.clang( ... )
       # Copy the C code into the user-defined --codeDir folder
       if cli.codeDir != Path"": cp cacheFile, cli.codeDir/relFile
+      #_____________________________
       # Compile the C code into binaries
       if cli.cmd != Compile: break SectionBin # Exit the block when not compiling in to binaries
       let flags = if cli.mode in {Release}: cfg.flags.release else: cfg.flags.debug
@@ -47,6 +55,7 @@ when isMainModule:
       let cc     = &"{cli.zigBin.string} cc {verb} {flags} {linkFl} {paths} {cacheFile.string} {cli.cfiles} -o {binFile.string}"
       dbg "Compiling into binary with command:  ",cc
       sh cc
+      #_____________________________
       # Run the final binary when requested
       if cli.run:
         dbg "Running the resulting binary:  ",binFile.string
