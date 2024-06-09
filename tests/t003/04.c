e9335cb60e995b29e46a78d9fa3de8538581e363
sOkam <7308253+heysokam@users.noreply.github.com>
2024-06-09 22:05:55 +0200

    tst: Better UnitTests for `include` statements

---
+++ tests/t003/04.c
@@ -0,0 +1,2 @@
+#include "./test/path/file1.h"
+#include "./test/path/file2.h"
---
+++ tests/t003/06.c
@@ -0,0 +1,3 @@
+static void one(void) { return; }
+static void two(void) { return; }
+static void thr(void) { return; }
