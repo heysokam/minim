9c53910cd78416e7788e418d48c0d00641045503
sOkam <7308253+heysokam@users.noreply.github.com>
2024-06-09 22:53:17 +0200

    doc: Better formatting for `include` UnitTests report message

--- tests/t003/test.nim
+++ tests/t003/test.nim
@@ -10,11 +10,11 @@ template tName:Path= thisDir.lastPathPart()
 #_______________________________________
 # @section Test
 #_____________________________
-test name "01 | Simple case"        : check "01"
-test name "02 | Global : @"         : check "02"
-test name "03 | Global : \"<...>\"" : check "03"
-test name "04 | Local  : .h"        : check "04"
-test name "05 | Local  : .c"        : check "05"
-test name "06 | Local  : .cm"       : check "06"
+test name "01 | Simple case"       : check "01"
+test name "02 | Global: @"         : check "02"
+test name "03 | Global: \"<...>\"" : check "03"
+test name "04 | Local: .h"         : check "04"
+test name "05 | Local: .c"         : check "05"
+test name "06 | Local: .cm"        : check "06"
 # test name "07 | Global : .cm"       : check "07"
 
