68647e4279343e9798620b61e6526fbb9d75748e
sOkam <7308253+heysokam@users.noreply.github.com>
2024-06-10 15:19:47 +0200

    tst: UnitTests for proc typedefs with complex types

--- tests/t009/test.nim
+++ tests/t009/test.nim
@@ -15,5 +15,6 @@ test name "02 | Multiword types" : check "02"
 test name "03 | Object: Basic"   : check "03"
 test name "04 | Object: Stub"    : check "04"
 # test name "05 | Object: Nested " : check "05"
-test name "06 | Proc: Basic "    : check "06"
+test name "06 | Proc: Basic"     : check "06"
+test name "07 | Proc: Complex"   : check "07"
 
