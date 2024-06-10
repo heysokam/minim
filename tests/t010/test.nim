2a7d04fb78212b5a3f805da2e3d1a2765f66d3e9
sOkam <7308253+heysokam@users.noreply.github.com>
2024-06-10 02:09:29 +0200

    tst: Affixes UnitTests skeleton folder

---
+++ tests/t010/test.nim
@@ -0,0 +1,14 @@
+const Title = "Affixes"
+#_______________________________________
+# @deps tests
+include ../base
+const thisDir = currentSourcePath.Path.parentDir()
+template tName:Path= thisDir.lastPathPart()
+#_______________________________________
+
+
+#_______________________________________
+# @section Test
+#_____________________________
+test name "00 | Dummy Test": check "00"
+
