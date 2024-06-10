1b0f54ac76200ca574d3cb4c947b0c39e99527ce
sOkam <7308253+heysokam@users.noreply.github.com>
2024-06-10 15:19:47 +0200

    tst: UnitTests for proc typedefs with complex types

---
+++ tests/t009/07.c
@@ -0,0 +1,3 @@
+typedef signed char* const (*Thing1)(unsigned long long int* one);
+typedef signed short u8 (*Thing2)(unsigned short i32* const two);
+typedef u32 const (*Thing3)(long long const* thr);
