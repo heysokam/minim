42acf6ae4701505857fd020060cc94109c779fbb
sOkam <7308253+heysokam@users.noreply.github.com>
2024-06-11 11:16:31 +0200

    new: Support for `{.fallthrough.}` pragma

--- tests/t004/test.nim
+++ tests/t004/test.nim
@@ -15,5 +15,6 @@ test name "02 | Warning"        : check "02"
 test name "03 | Emit"           : check "03"
 test name "04 | Namespace"      : check "04"
 test name "05 | Define"         : check "05"
-test name "06 | C Pragma: once" : check "06"
+test name "06 | Fallthough"     : check "06"
+test name "50 | C Pragma: once" : check "50"
 
