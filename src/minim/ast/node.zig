dbc7708a0a62a9794899d593ec460d255a9576b7
sOkam <7308253+heysokam@users.noreply.github.com>
2024-07-05 21:52:01 +0200

    new: Better `Ast.Node` base tools

--- src/minim/ast/node.zig
+++ src/minim/ast/node.zig
@@ -1,21 +1,29 @@
 //:_______________________________________________________________________
 //  ᛟ minim  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU LGPLv3 or later  :
 //:_______________________________________________________________________
+// @deps std
+const std = @import("std");
 // @deps zstd
 const zstd = @import("../../lib/zstd.zig");
 const Seq  = zstd.Seq;
 // @deps minim.ast
-const Func = @import("./func.zig");
+const Proc = @import("./proc.zig");
 
 
 /// @descr Describes a Top-Level Node of the language.
 pub const Node = union(enum) {
-  Func :Func,
+  Proc :Proc,
   pub const List = struct {
     data :?Seq(Node)= null,
 
+    /// @descr Creates a new empty Node.List object.
+    pub fn create(A :std.mem.Allocator) Node.List { return Node.List{.data= Seq(Node).init(A)}; }
+    /// @descr Releases all memory used by the Node.List
+    pub fn destroy(L:*Node.List) void { L.data.?.deinit(); }
     /// @descr Returns true if the Node list has no nodes.
     pub fn empty(L:*const Node.List) bool { return L.data == null; }
+    /// @descr Adds the given {@arg val} Node to the {@arg N} Node.List
+    pub fn append(N :*Node.List, val :Node) !void { try N.data.?.append(val); }
   };
 };
 
