37e0ceb78bc6e1ccf4c9d90d1cfdb854a7be9cdf
sOkam <7308253+heysokam@users.noreply.github.com>
2024-07-24 07:55:08 +0200

    git: Update package metadata at `info.zig`

--- info.zig
+++ info.zig
@@ -1,1 +1,8 @@
-const version = "0.0.0";
+//:_______________________________________________________________________
+//  ᛟ minim  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU LGPLv3 or later  :
+//:_______________________________________________________________________
+const version      = "0.0.0";
+const name         = "minim";
+const author       = "heysokam";
+const description  = "ᛟ minim | Minimalistic C Language";
+const license      = "LGPLv3-or-later";
