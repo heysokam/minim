b8aa855c82cf949e7bd9cc700f73d6e6100ce30b
sOkam <7308253+heysokam@users.noreply.github.com>
2024-08-28 08:56:45 +0200

    rew: Minimal logic for the `M` compiler entry point

--- src/M.zig
+++ src/M.zig
@@ -8,6 +8,7 @@ const zstd = @import("./lib/zstd.zig");
 // @deps minim
 const M   = @import("./minim.zig");
 const CLI = @import("./minim/cli.zig");
+const Cfg = @import("./minim/cfg.zig");
 
 
 //______________________________________
@@ -24,27 +25,34 @@ pub fn main() !void {
   defer cli.destroy();
 
   //_____________________________
-  // File Configuration
+  // Compile Process
   //_____________________________
   // Read the code
-  const zm = // TODO: Read from file
-    \\proc main *() :i32= return 42
-    \\
-    ;
+  const input = try zstd.files.read(cli.cfg.input, A.allocator(), .{.maxFileSize= cli.cfg.maxFileSize});
+  defer A.allocator().free(input);
   //_____________________________
   // Preprocess
+  const zm = try M.Pre.process(input, A.allocator());
+  defer A.allocator().free(zm);
   //_____________________________
   // Generate AST
-  const ast = try M.Ast.get(zm, A.allocator());
+  var ast = try M.Ast.get(zm, A.allocator());
+  defer ast.destroy();
   //_____________________________
   // Codegen
-  const code = try M.Gen.C(&ast);
-  if (cli.cfg.verbose) code.report();
+  var code = try M.Gen.C(&ast);
+  defer code.destroy();
+  //if (cli.cfg.verbose) code.report();
   //_____________________________
   // Write the code into a temp file
+  const tmpFile = Cfg.default.dir.cache++"/tmp"++Cfg.default.fmt.ext++".c";
+  try code.write(tmpFile);
+  // TODO: clangFmtBin clangFmtFile
   //_____________________________
-  // Compile the C code into binaries
+  // Compile the generated code into binaries
+  try M.CC.C(tmpFile, cli);
   //_____________________________
   // Run the final binary when requested
+  if (cli.cfg.run) try zstd.shell.run(&.{cli.cfg.output}, cli.A);
 }
 
