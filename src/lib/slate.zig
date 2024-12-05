90f317857de919f6f188f2754a9c32af16a0c8c7
sOkam <7308253+heysokam@users.noreply.github.com>
2024-12-05 14:24:14 +0100

    bld: Fix slate dependency module with confy

--- src/lib/slate.zig
+++ src/lib/slate.zig
@@ -1,4 +1,4 @@
 //:___________________________________________________________________
 //  *Slate  |  Copyright (C) Ivan Mar (sOkam!)  |  LGPLv3 or higher  :
 //:___________________________________________________________________
-pub usingnamespace @import("./slate/src/slate.zig");
+pub usingnamespace @import("slate");
