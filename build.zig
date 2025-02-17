e4dc5f9967510077cbae93a4ff2803ef97d0f8c9
sOkam <7308253+heysokam@users.noreply.github.com>
2025-02-17 02:20:31 +0100

    bld: Add `ztest` dependency as a zig module for zls

--- build.zig
+++ build.zig
@@ -22,6 +22,7 @@ pub fn build (B :*@import("std").Build) !void {
   // Dependencies: Project  (LSP support: zls)
   confy.root_module.addImport("zstd",  B.addModule("zstd",   .{.root_source_file= B.path("./bin/.lib/zstd/src/zstd.zig")   }));
   confy.root_module.addImport("slate", B.addModule("slate",  .{.root_source_file= B.path("./bin/.lib/slate/src/slate.zig") }));
+  confy.root_module.addImport("ztest", B.addModule("ztest",  .{.root_source_file= B.path("./bin/.lib/ztest/src/ztest.zig") }));
 
   // Add support for `zig build run (-- arg1 arg2 arg3)`
   const R = B.addRunArtifact(confy);
