ad6c894604b26d57a1fc1de352a4a37de2f01ce3
sOkam <7308253+heysokam@users.noreply.github.com>
2025-02-18 00:57:19 +0100

    bld: Basic Unit Tests for `minim.Tok`

--- src/tests/minim/tok/test.zig
+++ src/tests/minim/tok/test.zig
@@ -3,4 +3,57 @@
 //:_______________________________________________________________________
 // @deps std
 const std = @import("std");
-test "Tokenizer" { return error.SkipZigTest; }
+// @deps minim
+const M = @import("../../../minim.zig");
+const slate = @import("slate");
+// @deps minim.tests
+const t  = @import("../base.zig");
+const it = t.it;
+
+const Tokenizer = t.title("minim.Tok | General Cases");
+test Tokenizer { Tokenizer.begin(); defer Tokenizer.end();
+
+try it("should create the expected list of tokens for the Hello42 case", struct { fn f() !void {
+  // Setup
+  const code = t.case.Hello42.src;
+  var L = try slate.Lex.create(t.A, code);
+  defer L.destroy();
+  try L.process();
+  var T = try M.Tok.create(&L);
+  defer T.destroy();
+  // Validate
+  // Run
+  try T.process();
+  // Check
+  try t.eq(T.res.len, t.case.Hello42.res.tok.len);
+  for (T.res.items(.id), T.res.items(.loc), 0..) |tk, loc, id| {
+    const Expected = t.case.Hello42.res.tok[id];
+    try t.eq(tk,        Expected.id       );
+    try t.eq(loc.start, Expected.loc.start);
+    try t.eq(loc.end,   Expected.loc.end  );
+  }
+}}.f);
+
+try it("should create the expected list of tokens for the HelloIndentation case", struct { fn f() !void {
+  // Setup
+  const code = t.case.HelloIndentation.src;
+  var L = try slate.Lex.create(t.A, code);
+  defer L.destroy();
+  try L.process();
+  var T = try M.Tok.create(&L);
+  defer T.destroy();
+  // Validate
+  // Run
+  try T.process();
+  // Check
+  try t.eq(T.res.len, t.case.HelloIndentation.res.tok.len);
+  for (T.res.items(.id), T.res.items(.loc), 0..) |tk, loc, id| {
+    const Expected = t.case.HelloIndentation.res.tok[id];
+    try t.eq(tk,        Expected.id       );
+    try t.eq(loc.start, Expected.loc.start);
+    try t.eq(loc.end,   Expected.loc.end  );
+  }
+}}.f);
+
+} //:: minim.Tok | General Cases
+
