c57ab623b0b65cff643feafed49c224e6dc08bf1
sOkam <7308253+heysokam@users.noreply.github.com>
2025-02-21 07:36:36 +0100

    bld: Todo tests for Modules and Namespaces at `gen/t003`

---
+++ src/tests/minim/gen/t003/test.zig
@@ -0,0 +1,138 @@
+//:_______________________________________________________________________
+//  ᛟ minim  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU LGPLv3 or later  :
+//:_______________________________________________________________________
+// @deps minim
+const M = @import("../../../../minim.zig");
+// @deps minim.tests
+const t  = @import("../../base.zig");
+const it = t.it;
+
+var  Modules = t.title("Modules");
+test Modules { Modules.begin(); defer Modules.end();
+
+try t.hide.it("Include: Simple case", struct { fn f()!void {
+  const ID = "01";
+  const cm = @embedFile(ID++".cm");
+  const zm = @embedFile(ID++".zm");
+  const c  = @embedFile(ID++".c");
+  const z  = @embedFile(ID++".zig");
+  try t.not.eq_str(c,z);
+  try t.eq_str(cm,zm);
+
+  try t.check(cm, c, M.Lang.C);
+  // try check(zm, z, M.Lang.Zig); // TODO: Zig compilation support
+}}.f);
+
+try t.hide.it("Include: Global: @", struct { fn f()!void {
+  const ID = "02";
+  const cm = @embedFile(ID++".cm");
+  const zm = @embedFile(ID++".zm");
+  const c  = @embedFile(ID++".c");
+  const z  = @embedFile(ID++".zig");
+  try t.not.eq_str(c,z);
+  try t.eq_str(cm,zm);
+
+  try t.check(cm, c, M.Lang.C);
+  // try check(zm, z, M.Lang.Zig); // TODO: Zig compilation support
+}}.f);
+
+try t.hide.it("Include: Global: \"<...>\"", struct { fn f()!void {
+  const ID = "03";
+  const cm = @embedFile(ID++".cm");
+  const zm = @embedFile(ID++".zm");
+  const c  = @embedFile(ID++".c");
+  const z  = @embedFile(ID++".zig");
+  try t.not.eq_str(c,z);
+  try t.eq_str(cm,zm);
+
+  try t.check(cm, c, M.Lang.C);
+  // try check(zm, z, M.Lang.Zig); // TODO: Zig compilation support
+}}.f);
+
+try t.hide.it("Include: Local: .h", struct { fn f()!void {
+  const ID = "04";
+  const cm = @embedFile(ID++".cm");
+  const zm = @embedFile(ID++".zm");
+  const c  = @embedFile(ID++".c");
+  const z  = @embedFile(ID++".zig");
+  try t.not.eq_str(c,z);
+  try t.eq_str(cm,zm);
+
+  try t.check(cm, c, M.Lang.C);
+  // try check(zm, z, M.Lang.Zig); // TODO: Zig compilation support
+}}.f);
+
+try t.hide.it("Include: Local: .c", struct { fn f()!void {
+  const ID = "05";
+  const cm = @embedFile(ID++".cm");
+  const zm = @embedFile(ID++".zm");
+  const c  = @embedFile(ID++".c");
+  const z  = @embedFile(ID++".zig");
+  try t.not.eq_str(c,z);
+  try t.eq_str(cm,zm);
+
+  try t.check(cm, c, M.Lang.C);
+  // try check(zm, z, M.Lang.Zig); // TODO: Zig compilation support
+}}.f);
+
+try t.hide.it("Include: Local: .cm", struct { fn f()!void {
+  const ID = "06";
+  const cm = @embedFile(ID++".cm");
+  const zm = @embedFile(ID++".zm");
+  const c  = @embedFile(ID++".c");
+  const z  = @embedFile(ID++".zig");
+  try t.not.eq_str(c,z);
+  try t.eq_str(cm,zm);
+
+  try t.check(cm, c, M.Lang.C);
+  // try check(zm, z, M.Lang.Zig); // TODO: Zig compilation support
+}}.f);
+
+try t.hide.it("Include: Global: .cm", struct { fn f()!void {
+  const ID = "07";
+  const cm = @embedFile(ID++".cm");
+  const zm = @embedFile(ID++".zm");
+  const c  = @embedFile(ID++".c");
+  const z  = @embedFile(ID++".zig");
+  try t.not.eq_str(c,z);
+  try t.eq_str(cm,zm);
+
+  try t.check(cm, c, M.Lang.C);
+  // try check(zm, z, M.Lang.Zig); // TODO: Zig compilation support
+}}.f);
+
+// # TODO: Imports   (at #50)
+}
+
+
+var  Namespaces = t.title("Namespaces");
+test Namespaces { Namespaces.begin(); defer Namespaces.end();
+
+try t.hide.it("Blocks: Simple case", struct { fn f()!void {
+  const ID = "90";
+  const cm = @embedFile(ID++".cm");
+  const zm = @embedFile(ID++".zm");
+  const c  = @embedFile(ID++".c");
+  const z  = @embedFile(ID++".zig");
+  try t.not.eq_str(c,z);
+  try t.eq_str(cm,zm);
+
+  try t.check(cm, c, M.Lang.C);
+  // try check(zm, z, M.Lang.Zig); // TODO: Zig compilation support
+}}.f);
+
+try t.hide.it("Blocks: Named", struct { fn f()!void {
+  const ID = "91";
+  const cm = @embedFile(ID++".cm");
+  const zm = @embedFile(ID++".zm");
+  const c  = @embedFile(ID++".c");
+  const z  = @embedFile(ID++".zig");
+  try t.not.eq_str(c,z);
+  try t.eq_str(cm,zm);
+
+  try t.check(cm, c, M.Lang.C);
+  // try check(zm, z, M.Lang.Zig); // TODO: Zig compilation support
+}}.f);
+
+}
+
