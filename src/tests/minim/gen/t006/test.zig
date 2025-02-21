d3dfa8e8b11f77993ad959ee9fbbfd3146b6cf86
sOkam <7308253+heysokam@users.noreply.github.com>
2025-02-21 07:48:27 +0100

    bld: Todo tests for Comments at `gen/t006`

---
+++ src/tests/minim/gen/t006/test.zig
@@ -0,0 +1,53 @@
+//:_______________________________________________________________________
+//  ᛟ minim  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU LGPLv3 or later  :
+//:_______________________________________________________________________
+// @deps minim
+const M = @import("../../../../minim.zig");
+// @deps minim.tests
+const t  = @import("../../base.zig");
+const it = t.it;
+
+var  Comments = t.title("Comments and Newlines");
+test Comments { Comments.begin(); defer Comments.end();
+
+try t.hide.it("Basic Doc Comment", struct { fn f()!void {
+  const ID = "01";
+  const cm = @embedFile(ID++".cm");
+  const zm = @embedFile(ID++".zm");
+  const c  = @embedFile(ID++".c");
+  const z  = @embedFile(ID++".zig");
+  try t.not.eq_str(c,z);
+  try t.eq_str(cm,zm);
+
+  try t.check(cm, c, M.Lang.C);
+  // try check(zm, z, M.Lang.Zig); // TODO: Zig compilation support
+}}.f);
+
+try t.hide.it("Multi-line Comments", struct { fn f()!void {
+  const ID = "02";
+  const cm = @embedFile(ID++".cm");
+  const zm = @embedFile(ID++".zm");
+  const c  = @embedFile(ID++".c");
+  const z  = @embedFile(ID++".zig");
+  try t.not.eq_str(c,z);
+  try t.eq_str(cm,zm);
+
+  try t.check(cm, c, M.Lang.C);
+  // try check(zm, z, M.Lang.Zig); // TODO: Zig compilation support
+}}.f);
+
+try t.hide.it("Inline Comments", struct { fn f()!void {
+  const ID = "03";  // TODO:
+  const cm = @embedFile(ID++".cm");
+  const zm = @embedFile(ID++".zm");
+  const c  = @embedFile(ID++".c");
+  const z  = @embedFile(ID++".zig");
+  try t.not.eq_str(c,z);
+  try t.eq_str(cm,zm);
+
+  try t.check(cm, c, M.Lang.C);
+  // try check(zm, z, M.Lang.Zig); // TODO: Zig compilation support
+}}.f);
+
+}
+
