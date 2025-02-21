ebda81f970e8491beeb7cde2f304eda9fab9f041
sOkam <7308253+heysokam@users.noreply.github.com>
2025-02-21 07:45:12 +0100

    bld: Todo tests for Discard at `gen/t005`

---
+++ src/tests/minim/gen/t005/test.zig
@@ -0,0 +1,40 @@
+//:_______________________________________________________________________
+//  ᛟ minim  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU LGPLv3 or later  :
+//:_______________________________________________________________________
+// @deps minim
+const M = @import("../../../../minim.zig");
+// @deps minim.tests
+const t  = @import("../../base.zig");
+const it = t.it;
+
+var  Discard = t.title("Discard");
+test Discard { Discard.begin(); defer Discard.end();
+
+try t.hide.it("Single identifier", struct { fn f()!void {
+  const ID = "01";
+  const cm = @embedFile(ID++".cm");
+  const zm = @embedFile(ID++".zm");
+  const c  = @embedFile(ID++".c");
+  const z  = @embedFile(ID++".zig");
+  try t.not.eq_str(c,z);
+  try t.eq_str(cm,zm);
+
+  try t.check(cm, c, M.Lang.C);
+  // try check(zm, z, M.Lang.Zig); // TODO: Zig compilation support
+}}.f);
+
+try t.hide.it("Multi identifier", struct { fn f()!void {
+  const ID = "02";
+  const cm = @embedFile(ID++".cm");
+  const zm = @embedFile(ID++".zm");
+  const c  = @embedFile(ID++".c");
+  const z  = @embedFile(ID++".zig");
+  try t.not.eq_str(c,z);
+  try t.eq_str(cm,zm);
+
+  try t.check(cm, c, M.Lang.C);
+  // try check(zm, z, M.Lang.Zig); // TODO: Zig compilation support
+}}.f);
+
+}
+
