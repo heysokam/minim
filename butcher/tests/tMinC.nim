c9f7ed08929b35ce4dad2706dbd36f08e9d38b3e
sOkam <7308253+heysokam@users.noreply.github.com>
2025-02-21 08:46:48 +0100

    bld: Move all `gen` todo tests to `gen/todo`

--- butcher/tests/tMinC.nim
+++
@@ -1,7 +0,0 @@
-# @deps std
-from std/os import nil
-# @deps ndk
-import nstd/modules
-# Import all tests recursively
-const thisDir = os.parentDir(currentSourcePath())
-modules.importAllRec( thisDir, ["test.nim"] )
--- src/tests/minim/gen/test.zig
+++ src/tests/minim/gen/test.zig
@@ -10,14 +10,15 @@ test {
   _ = @import("./t000/test.zig"); // Basic Codegen
   _ = @import("./t001/test.zig"); // Procs
   _ = @import("./t002/test.zig"); // Variables
-  _ = @import("./t003/test.zig"); // Modules and Namespaces
-  _ = @import("./t004/test.zig"); // Standalone Pragmas
-  _ = @import("./t005/test.zig"); // Discard
-  _ = @import("./t006/test.zig"); // Comments and Newlines
-  _ = @import("./t007/test.zig"); // Control Flow
-  _ = @import("./t008/test.zig"); // Calls
-  _ = @import("./t009/test.zig"); // Types
-  _ = @import("./t010/test.zig"); // Affixes
-  _ = @import("./t011/test.zig"); // Cast
+  // TODO:
+  // _ = @import("./t003/test.zig"); // Modules and Namespaces
+  // _ = @import("./t004/test.zig"); // Standalone Pragmas
+  // _ = @import("./t005/test.zig"); // Discard
+  // _ = @import("./t006/test.zig"); // Comments and Newlines
+  // _ = @import("./t007/test.zig"); // Control Flow
+  // _ = @import("./t008/test.zig"); // Calls
+  // _ = @import("./t009/test.zig"); // Types
+  // _ = @import("./t010/test.zig"); // Affixes
+  // _ = @import("./t011/test.zig"); // Cast
 }
 
