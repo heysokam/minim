75781565d78548e31a0085cf1de7908d5b79e8f3
sOkam <7308253+heysokam@users.noreply.github.com>
2025-03-31 16:22:32 +0200

    git: Remove confy submodule

--- src/lib/confy.zig
+++
@@ -1,5 +0,0 @@
-//:_______________________________________________________
-//  ᛝ confy  |  Copyright (C) Ivan Mar (sOkam!)  |  MIT  :
-//:_______________________________________________________
-// @fileoverview Cable connector to the confy library
-pub usingnamespace @import("./confy/src/confy.zig");
