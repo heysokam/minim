e0e6e159c770ee38e8012002c2a377b32f6aeeab
sOkam <7308253+heysokam@users.noreply.github.com>
2025-05-25 02:25:48 +0200

    bld: Fix errors for zls unable to interpret `build.zig`

--- build.zig
+++ build.zig
@@ -6,9 +6,16 @@
 //!  This file is not needed at all to build or use the project
 //_________________________________________________________________|
 pub fn build (B :*@import("std").Build) !void {
+  const minim = B.addExecutable(.{
+    .name             = "minim",
+    .root_source_file = B.path("./src/minim.zig"),
+    .target           = B.standardTargetOptions(.{}),
+    .optimize         = B.standardOptimizeOption(.{}),
+  });
+  B.installArtifact(minim);
   // Dependencies: Project  (LSP support: zls)
-  B.addModule("zstd",     .{.root_source_file= B.path("./bin/.lib/zstd/src/zstd.zig")});
-  B.addModule("slate",    .{.root_source_file= B.path("./bin/.lib/slate/src/slate.zig") });
-  B.addModule("minitest", .{.root_source_file= B.path("./bin/.lib/minitest/src/minitest.zig") });
+  minim.root_module.addImport("zstd",  B.addModule("zstd",   .{.root_source_file= B.path("./bin/.lib/zstd/src/zstd.zig")   }));
+  minim.root_module.addImport("slate", B.addModule("slate",  .{.root_source_file= B.path("./bin/.lib/slate/src/slate.zig") }));
+  minim.root_module.addImport("minitest", B.addModule("minitest",  .{.root_source_file= B.path("./bin/.lib/minitest/src/minitest.zig") }));
 }
 
